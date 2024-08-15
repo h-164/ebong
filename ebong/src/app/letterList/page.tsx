8c15774c23abe313bf27e8f7bf12fda8ecaadb4f
suyeon <moa0823@naver.com>
2024-08-15 20:51:58 +0900

    feat : 편지 조회 페이지 생성 및 api 호출 #2

---
+++ ebong/src/app/letterList/page.tsx
@@ -0,0 +1,28 @@
+export default async function WriteList() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/letters`,
+    { cache: "no-store" }
+  );
+  const data = await response.json();
+  const lettersArray = data.data.letters;
+
+  return (
+    <>
+      {lettersArray?.map((letter: any) => (
+        <>
+          <h3>to.{letter.recipient}</h3>
+          <p>{letter.letterContent}</p>
+          <p>{letter.date}</p>
+          <h3>from.{letter.sender}</h3>
+          {letter.isReplied && (
+            <>
+              <p>
+                ↳{letter.recipient}:{letter.replyContent}
+              </p>
+            </>
+          )}
+        </>
+      ))}
+    </>
+  );
+}
