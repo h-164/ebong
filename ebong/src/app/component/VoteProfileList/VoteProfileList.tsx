efdfa6fd3ae14c7191c5f460a308a3ddd7b9885d
suyeon <moa0823@naver.com>
2024-09-13 23:27:13 +0900

    feat : 투표수 동률 처리 #15

--- ebong/src/app/component/VoteProfileList/VoteProfileList.tsx
+++ ebong/src/app/component/VoteProfileList/VoteProfileList.tsx
@@ -3,6 +3,18 @@
 import Image from "next/image";
 import { useVoteProfileList, useVote } from "./VoteProfileListHooks";
 
+interface Profile {
+  _id: string;
+  smilingImg: string;
+  name: string;
+  introduction: string;
+  voteCount: number;
+}
+
+interface GroupedProfiles {
+  [rank: string]: Profile[];
+}
+
 export const VoteProfileList = () => {
   const { data } = useVoteProfileList();
   const voteProfiles = data?.vote_profiles;
@@ -13,20 +25,51 @@ export const VoteProfileList = () => {
     voteMutation.mutate(_id);
   };
 
+  let rank = [1, 2, 3, 4, 5, 6, 7];
+  voteProfiles?.map((profile, index) => {
+    if (index > 0 && profile.voteCount === voteProfiles[index - 1].voteCount) {
+      rank[index] = rank[index - 1];
+    }
+    return null;
+  });
+
+  const groupedProfiles: GroupedProfiles = {};
+
+  voteProfiles?.forEach((profile, index) => {
+    const rankValue = rank[index].toString();
+    if (!groupedProfiles[rankValue]) {
+      groupedProfiles[rankValue] = [];
+    }
+    groupedProfiles[rankValue].push(profile);
+  });
+
   return (
     <>
-      {voteProfiles?.map((profile) => {
-        const { _id, smilingImg, name, introduction, voteCount } = profile;
-        return (
-          <div key={_id}>
-            <Image src={smilingImg} alt="profileImg" width={100} height={100} />
-            <h1>{name}</h1>
-            <p>{introduction}</p>
-            <p>{voteCount}</p>
-            <button onClick={() => handleVote(_id)}>투표하기</button>
+      {Object.keys(groupedProfiles).map((rankValue) => (
+        <div key={rankValue} style={{ display: "flex" }}>
+          <div>
+            <h1>{rankValue}등</h1>
           </div>
-        );
-      })}
+          {groupedProfiles[rankValue].map((profile) => {
+            const { _id, smilingImg, name, introduction, voteCount } = profile;
+            return (
+              <div key={_id}>
+                <Image
+                  src={smilingImg}
+                  alt="profileImg"
+                  width={100}
+                  height={100}
+                />
+                <h1>{name}</h1>
+                <p>{introduction}</p>
+                <p>{voteCount}</p>
+                <button onClick={() => handleVote(_id)}>투표하기</button>
+              </div>
+            );
+          })}
+          <h2>{groupedProfiles[rankValue][0].voteCount}표</h2>
+        </div>
+      ))}
     </>
   );
 };
