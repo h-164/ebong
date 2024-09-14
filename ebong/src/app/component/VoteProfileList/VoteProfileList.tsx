e55ddf6ea6903f6835cea314d40cb3a8ff7acae3
suyeon <moa0823@naver.com>
2024-09-14 22:48:19 +0900

    feat : 투표하기 버튼 클릭 시 성공 여부 모달 띄우기 #15

--- ebong/src/app/component/VoteProfileList/VoteProfileList.tsx
+++ ebong/src/app/component/VoteProfileList/VoteProfileList.tsx
@@ -2,6 +2,8 @@
 
 import Image from "next/image";
 import { useVoteProfileList, useVote } from "./VoteProfileListHooks";
+import { SharedModal } from "../SharedModal/SharedModal";
+import { useModal } from "../SharedModal/sharedModal.hooks";
 
 interface Profile {
   _id: string;
@@ -19,7 +21,21 @@ export const VoteProfileList = () => {
   const { data } = useVoteProfileList();
   const voteProfiles = data?.vote_profiles;
 
-  const voteMutation = useVote();
+  const {
+    isModalOpen: isSuccessModalOpen,
+    openModal: openSuccessModal,
+    closeModal: closeSuccessModal,
+    message: successMessage,
+  } = useModal();
+
+  const {
+    isModalOpen: isErrorModalOpen,
+    openModal: openErrorModal,
+    closeModal: closeErrorModal,
+    message: errorMessage,
+  } = useModal();
+
+  const voteMutation = useVote(openSuccessModal, openErrorModal);
 
   const handleVote = (_id: string) => {
     voteMutation.mutate(_id);
@@ -62,7 +78,6 @@ export const VoteProfileList = () => {
                 />
                 <h1>{name}</h1>
                 <p>{introduction}</p>
-                <p>{voteCount}</p>
                 <button onClick={() => handleVote(_id)}>투표하기</button>
               </div>
             );
@@ -70,6 +85,22 @@ export const VoteProfileList = () => {
           <h2>{groupedProfiles[rankValue][0].voteCount}표</h2>
         </div>
       ))}
+      <SharedModal
+        imgUrl="https://drive.google.com/uc?export=view&id=149XDtE4x1iVD8JaNgBbOZSjUiVYsDS2Y"
+        message={successMessage}
+        isModalOpen={isSuccessModalOpen}
+        closeModal={closeSuccessModal}
+        leftButton={true}
+        leftButtonMessage="확인"
+      />
+      <SharedModal
+        imgUrl="https://drive.google.com/uc?export=view&id=149XDtE4x1iVD8JaNgBbOZSjUiVYsDS2Y"
+        message={errorMessage}
+        isModalOpen={isErrorModalOpen}
+        closeModal={closeErrorModal}
+        leftButton={true}
+        leftButtonMessage="확인"
+      />
     </>
   );
 };
--- ebong/src/app/component/VoteProfileList/VoteProfileListHooks.ts
+++ ebong/src/app/component/VoteProfileList/VoteProfileListHooks.ts
@@ -8,13 +8,22 @@ export const useVoteProfileList = () => {
   });
 };
 
-export const useVote = () => {
+export const useVote = (
+  openSuccessModal: (message: string) => void,
+  openErrorModal: (message: string) => void
+) => {
   const queryClient = useQueryClient();
-
+  
   return useMutation({
     mutationFn: (_id: string) => voteProfileClientApi.patchVoteProfiles(_id),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["voteProfiles"] });
+      openSuccessModal(
+        "투표 완료!"
+      );
+    },
+    onError: (error: any) => {
+      openErrorModal("투표 실패ㅠ 잠시 후 다시 시도해주세요");
     },
   });
 };
