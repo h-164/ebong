40558144168df46739d147a023213ba603c43871
suyeon <moa0823@naver.com>
2024-10-08 01:14:47 +0900

    fix : 날짜 포맷 처리 방식 변경 #19

--- ebong/src/models/letter.schema.ts
+++ ebong/src/models/letter.schema.ts
@@ -1,21 +1,37 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
+
+const currentUTCDate = () => dayjs().utc().toDate();
 
 export const LetterSchema = new Schema({
-    sender:String,
-    recipient:String,
-    letterContent:String,
-    isReplied:{
-        type:Boolean, 
-        default:false
-    },
-    replyContent:{
-        type:String,
-        default:""},
-    date:{
-        type:Date,
-        default:Date.now
-    }
-})
+  sender: {
+    type: String,
+    required: true,
+  },
+  recipient: {
+    type: String,
+    required: true,
+  },
+  letterContent: {
+    type: String,
+    required: true,
+  },
+  isReplied: {
+    type: Boolean,
+    default: false,
+  },
+  replyContent: {
+    type: String,
+    default: "",
+  },
+  date: {
+    type: Date,
+    default: currentUTCDate, 
+  },
+});
 
 
-export const LETTER = mongoose.models.Letter?? mongoose.model("Letter", LetterSchema);
+export const LETTER = mongoose.models.Letter ?? mongoose.model("Letter", LetterSchema);
